1b6a3fd25a0b4fbcb11d6d4879ad6eb618aa5fbe
Himani Lotia <Himani Lotia>
2016-10-18 17:42:05 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+// app.use((request, response, next) => {
+//   response.render('maintanence.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+//  response.send('<h1>Hello Express!</h1>');
+response.render('home.hbs', {
+  pageTitle: 'Home Page',
+  welcomeMessage: 'Welcome to my Website',
+
+})
+});
+
+app.get('/about', (request, response) => {
+  response.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (request, response) => {
+  response.send({
+    errorMessage: 'Unable to handle request'
+  });
+})
+app.listen(3000, () => {
+  console.log('server is up');
+});
